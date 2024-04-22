9f56e719e550f515fd6ae3ec3fa981512ce57315
hellen <jameshellen2241@gmail.com>
2024-04-22 14:45:10 +0300

    included changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,36 @@ import {Link} from "react-router-dom"
 function Footer(){
     return(
         <footer>
-        <div >
+        <div className="flex">
+
+           <div>
+           <h1 className="text-2xl text-[#0D47A1] text-center">Designers Opportunity</h1>
+           </div>
+           <div>
            <Link to="/"> <img src="src/assets/images/logo.png"width={200}  /></Link>
-           <h1 className="text-2xl text-[#0D47A1g]">Designers Opportunity</h1>
+           <p className="w-58">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dignissimos nulla tenetur, ut fugit molestias unde excepturi nobis voluptas, obcaecati laudantium? Non voluptates assumenda dolore eius numquam placeat tempora debitis.</p>
+           </div>
+           <div>
+          <h2 className="font-bold">Quick Links</h2>
+          <ul>
+            <li>Home </li>
+            <li>Log In</li>
+            <li>Sign Up</li>
+          </ul>
+        </div>
+           <div>
+          <h2 className="font-bold">More Info</h2>
+          <ul>
+            <li>Housing Finshing </li>
+            <li>Painting</li>
+            <li>Structure</li>
+            <li>Modifying</li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="font-bold">Contact Us</h2>
+         
+        </div>
         </div>
         </footer>
     )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,11 @@ function Hero(){
     return(
         <section>
         <div className=" justify-between items-center ">
-            <div>
-                <h1>Pick the best designers that enables the finishing of buildings be great. </h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi facere vero eius, temporibus veniam ratione libero amet itaque veritatis, sunt iste. Molestiae laboriosam dolorum eos? Sunt culpa repellat delectus accusamus.</p>
+                       <div className="bg-[url('./src/assets/images/Home-Decor-Sim-Boon-Yang-BT20210924-XSA-015-00hero.avif')] h-[50vh]">
+                <h1 className="text-[#0D47A1] text-2xl">Pick the best designers that enables the finishing of buildings be great. </h1>
+                <p className="text-[#0D47A1] text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi facere vero eius, temporibus veniam ratione libero amet itaque veritatis, sunt iste. Molestiae laboriosam dolorum eos? Sunt culpa repellat delectus accusamus.</p>
             </div>
-            <div> <img className="z-[-1]absolute top-0 right-0 w-[100%] h-[50vh]" src="src/assets/images/Home-Decor-Sim-Boon-Yang-BT20210924-XSA-015-00hero.avif"/>
-                 
-            </div>
-
+           
         </div>
         </section>
     )
